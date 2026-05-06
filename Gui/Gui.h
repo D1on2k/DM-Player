9a8358629f6c0ff19835600f19d453697941c849
Dion2k <101985983+D1on2k@users.noreply.github.com>
2026-05-06 20:02:51 +0300

    Update Gui.h

--- Gui/Gui.h
+++ Gui/Gui.h
@@ -7,6 +7,7 @@
 #include "ImGui/imgui.h"
 #include <d3d11.h>
 #include "stb_image.h"
+#include <string>
 
 ID3D11Device* g_pd3dDevice = nullptr;
 ID3D11DeviceContext* g_pd3dDeviceContext = nullptr;
@@ -15,7 +16,14 @@ bool g_SwapChainOccluded = false;
 UINT g_ResizeWidth = 0, g_ResizeHeight = 0;
 ID3D11RenderTargetView* g_mainRenderTargetView = nullptr;
 int selectedIndex = -1;
+
+bool test = true;
+
 static int Tabsystem = 0;
+static int LibraryTabSystem = 0;
+
+//std::string HoldSearch = "";
+char HoldSearch[128] = ""; // Make it char because compiler hates string
 
 bool CreateDeviceD3D(HWND hWnd);
 void CleanupDeviceD3D();
