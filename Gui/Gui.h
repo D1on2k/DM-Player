beadb2a85f1d2b9ac4a0ba3a0118aa2fe62d47c8
Dion2k <101985983+D1on2k@users.noreply.github.com>
2026-05-10 16:41:16 +0300

    Update Gui.h

--- Gui/Gui.h
+++ Gui/Gui.h
@@ -2,12 +2,21 @@
 
 #define STB_IMAGE_IMPLEMENTATION
 
-#include <iostream>
 #include <windows.h>
-#include "ImGui/imgui.h"
 #include <d3d11.h>
-#include "stb_image.h"
+#include <tlhelp32.h>
 #include <string>
+#include <iostream>
+#include "ImGui/imgui.h"
+#include "ImGui/imconfig.h"
+#include "ImGui/backends/imgui_impl_win32.h"
+#include "ImGui/backends/imgui_impl_dx11.h"
+#include "ImGui/ImGui.h"
+#include "ImGui/imgui_internal.h"
+#include "stb_image.h"
+#include "Searching/searching.h"
+#include "PathTab4/select_path.h"
+
 
 ID3D11Device* g_pd3dDevice = nullptr;
 ID3D11DeviceContext* g_pd3dDeviceContext = nullptr;
@@ -16,6 +25,7 @@ UINT g_ResizeWidth = 0, g_ResizeHeight = 0;
 ID3D11RenderTargetView* g_mainRenderTargetView = nullptr;
 
 static bool idk = true;
+static bool scan = false;
 bool g_SwapChainOccluded = false;
 bool test = true;
 bool g_IsMaximized = false;
@@ -29,6 +39,7 @@ int selectedIndex = -1;
 
 //std::string HoldSearch = "";
 char HoldSearch[128] = ""; // Make it char because compiler hates string
+char pathBuffer[512] = {};
 extern char IAMTIRED[256];
 
 bool CreateDeviceD3D(HWND hWnd);
