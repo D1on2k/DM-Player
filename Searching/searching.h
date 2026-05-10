595cb30d34cc5f235f4791293e29206849af2ad8
Dion2k <101985983+D1on2k@users.noreply.github.com>
2026-05-10 19:35:40 +0300

    Update searching.h

--- Searching/searching.h
+++ Searching/searching.h
@@ -1,10 +1,12 @@
 #pragma once
+
+#define _HAS_STD_BYTE 0
+
 #include <string>
 
-using namespace std;
-
-extern string takepath;
-extern string searchfortitle;
-extern string searchpathformusic; 
+extern std::string takepath;
+extern std::string searchfortitle;
+extern std::string searchpathformusic;
+extern std::string foldiername;
 
 void findimages();
