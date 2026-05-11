c975b1a1d2ca71c521d6677b39613780a776d32c
Dion2k <101985983+D1on2k@users.noreply.github.com>
2026-05-11 21:44:01 +0300

    Update searching.h

--- Searching/searching.h
+++ Searching/searching.h
@@ -1,8 +1,12 @@
+// searching.h
+
 #pragma once
 
 #define _HAS_STD_BYTE 0
 
 #include <string>
+#include "PathTab4/select_path.h"
+#include <utility>
 
 extern std::string takepath;
 extern std::string searchfortitle;
@@ -10,3 +14,4 @@ extern std::string searchpathformusic;
 extern std::string foldiername;
 
 void findimages();
+bool idk12123(const std::string& folderName, std::string& artist, std::string& album);
