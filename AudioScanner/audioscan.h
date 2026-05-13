f9cdb15930bc9c6028d8ff05c8367031bc949648
Dion2k <101985983+D1on2k@users.noreply.github.com>
2026-05-13 20:17:18 +0300

    Add files via upload

---
+++ AudioScanner/audioscan.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <filesystem>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <cctype>
+#include "Gui/Gui.h"
+
+void scanfiles();
+void ldsngsifneeded();
