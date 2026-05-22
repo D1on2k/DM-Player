fec2d2831763ec9f46eb7e438eebb03cefe922af
Dion2k <101985983+D1on2k@users.noreply.github.com>
2026-05-22 07:43:22 +0300

    Update select_path.cpp

--- PathTab4/select_path.cpp
+++ PathTab4/select_path.cpp
@@ -22,71 +22,15 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
-#include "select_path.h"
+#pragma once
 
-using namespace std;
+#include <windows.h>
+#include <shlobj.h>
+#include <fstream>
+#include <string>
 
-string FoldierPath = "";
+extern std::string FoldierPath;
 
-// Helper to convert wide string to normal string (we need this sometimes)
-string WStringToString(const wstring& wstr)
-{
-    if (wstr.empty()) return "";
-    
-    int size = WideCharToMultiByte(CP_UTF8, 0, wstr.c_str(), -1, NULL, 0, NULL, NULL);
-    string result(size, 0);
-    WideCharToMultiByte(CP_UTF8, 0, wstr.c_str(), -1, &result[0], size, NULL, NULL);
-    result.resize(size - 1);
-    return result;
-}
-
-
-bool SelectMusicFolder()
-{
-    // Setup the folder dialog
-    
-    BROWSEINFOA bi = { 0 };
-    bi.lpszTitle = "Select Folder";
-    bi.ulFlags = BIF_RETURNONLYFSDIRS | BIF_NEWDIALOGSTYLE;   // Nice looking dialog
-
-    // Open the folder picker
-    LPITEMIDLIST pidl = SHBrowseForFolderA(&bi);
-    
-    if (pidl != nullptr)
-    {
-        char path[MAX_PATH] = { 0 };
-        
-        if (SHGetPathFromIDListA(pidl, path))
-        {
-            FoldierPath = path; // Save the selected path
-            SaveMusicPath(); // Save it to file so it remembers next time
-            CoTaskMemFree(pidl);
-            return true;
-        }
-        CoTaskMemFree(pidl);
-    }
-    
-    return false;
-}
-
-void SaveMusicPath()
-{
-    ofstream file("music_path.txt");
-    
-    if (file.is_open())
-    {
-        file << FoldierPath;
-        file.close();
-    }
-}
-
-void LoadSavedMusicPath()
-{
-    ifstream file("music_path.txt");
-    
-    if (file.is_open())
-    {
-        getline(file, FoldierPath);
-        file.close();
-    }
-}
+bool SelectMusicFolder();
+void LoadSavedMusicPath();
+void SaveMusicPath();
