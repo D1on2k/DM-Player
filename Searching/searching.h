17152d8ad3583c41d510e24e3c3933eb2b4546fb
Dion2k <101985983+D1on2k@users.noreply.github.com>
2026-05-31 17:01:54 +0300

    Update searching.h

--- Searching/searching.h
+++ Searching/searching.h
@@ -29,6 +29,8 @@ SOFTWARE.
 #include <string>
 #include "PathTab4/select_path.h"
 #include <utility>
+#include <filesystem>
+#include <vector>
 
 extern std::string takepath;
 extern std::string searchfortitle;
@@ -38,3 +40,4 @@ extern std::string foldiername;
 void findimages();
 bool idk12123(const std::string& folderName, std::string& artist, std::string& album);
 std::string FormatTime(float seconds);
+std::vector<std::string> searchinfoldier(const std::string& folderPath);
